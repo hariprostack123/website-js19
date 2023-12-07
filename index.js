5416b4f05716295b2abcbbe35069aad8a4874ffd
hariprostack123 <128031941+hariprostack123@users.noreply.github.com>
2023-12-07 12:28:54 +0530

    loop

---
+++ index.js
@@ -0,0 +1,15 @@
+// while loop = repeat some code
+//              while some condition is true
+//              potentially infinite
+
+/*let userName = "";
+
+while(userName == "" || userName == null){
+    userName = window.prompt("Enter your name");
+}
+
+console.log("Hello", userName);
+*/
+while(1 == 1){
+    console.log("HELP! I'M STUCK IN AN INFINITE LOOP!");
+}
